54ca86a929e86e6d80161d151cae8b994e9d35ab
Fede Messina <federico.messina@hotmail.com>
2023-11-16 15:50:53 -0300

    me trae el array del carrito pero no me lo renderiza lpm

--- src/context/ShoppingCartContex.jsx
+++ src/context/ShoppingCartContex.jsx
@@ -2,49 +2,64 @@ import { createContext, useState } from "react";
 
 export const CartContext = createContext(null);
 
-export const ShoppingCartProvider = ({
-    children})=> {
+export const ShoppingCartProvider = ({ children }) => {
+  const [carrito, setCarrito] = useState([]);
+  const [badge, setBadge] = useState(0);
+
+  const agregarProducto = (producto, count) => {
+    alert(`vas a agregar ${count} unidades de ${producto.nombre} a tu carrito`);
     
-    const [carrito, setCarrito] = useState([])
-    const [badge, setBadge] = useState(0)
+    const {id, nombre,precio } = producto;
+    const productoExistente = carrito.find((p) => p.id === producto.id);
     
-    const agregarProducto = (producto, count) =>{
-        alert(`vas a agregar ${count} unidades de ${producto.nombre} a tu carrito`)
-        
-        const productoExistente = carrito.find((p)=>p.id ===producto.id );
-        if (productoExistente > 0 ){
+    if (productoExistente) {
             const actualizarCarrito = [...carrito]
             actualizarCarrito(productoExistente).count += count
             setCarrito(actualizarCarrito)
-            console.log(producto)
+            
+        } 
+        else{
+            const nuevoProducto = {id, nombre, precio, count}
+            const carritoNuevo = [...carrito, nuevoProducto]
+            setCarrito(carritoNuevo)
+            
         }
+  };
+
+  const borrarProducto = (idProducto) => {
+    const nuevoCarrito = carrito.filter(
+      (producto) => producto.id !== idProducto
+    );
+    setCarrito(nuevoCarrito);
+  };
+
+  const vaciarCarrito = () => {
+    setCarrito([]);
+  };
+
+  const cantidadProductosCarrito = () => {
+    if (carrito.length > 0) {
+      const itemsCarrito = carrito.length;
+      setBadge(itemsCarrito);
     }
+  };
 
-    const borrarProducto = (idProducto) => {
-        const nuevoCarrito = carrito.filter(producto => producto.id !== idProducto)
-        setCarrito(nuevoCarrito)
-    }
-    
-    const vaciarCarrito = ()=>{
-        setCarrito([]);
-    }
+  return (
+    <CartContext.Provider
+      value={{
+        carrito,
+        setCarrito,
+        badge,
+        setBadge,
+        agregarProducto,
+        borrarProducto,
+        cantidadProductosCarrito,
+        vaciarCarrito,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
 
-    const cantidadProductosCarrito = () => {
-        if(carrito.length > 0){
-            const itemsCarrito = carrito.length
-            setBadge(itemsCarrito)
-        }
-
-    }
-
-    return(
-
-        <CartContext.Provider value={{carrito, setCarrito, badge, setBadge, agregarProducto , borrarProducto, cantidadProductosCarrito, vaciarCarrito}}>
-            {children}
-        </CartContext.Provider>
-    )
-
-
-}
-
-export default ShoppingCartProvider 
+export default ShoppingCartProvider;
